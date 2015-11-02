0c57e9106621605907e2d1d3ad552c254b6ec375
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-11-02 12:43:30 -0500

    fixed phone number formatting

--- EthiopiaSMS/twilio_helper.py
+++ EthiopiaSMS/twilio_helper.py
@@ -8,7 +8,7 @@ client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 def send_message(body, to_number):
   message = client.calls.create(
     from_= FROM_NUMBER,
-    to = "+1" + to_number,
+    to = to_number,
     url = "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient" 
     )
   # function will log this into a database
