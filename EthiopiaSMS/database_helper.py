640c9ee4514d50f1760aeb7ea2326571934a4d85
Eunice Kokor <eek2138@columbia.edu>
2016-04-26 16:36:12 -0400

    fix to question string

--- EthiopiaSMS/database_helper.py
+++ EthiopiaSMS/database_helper.py
@@ -72,7 +72,7 @@ def add_call_to_db(user_id, call_id, *question, **answer):
             else:
               if answer == "None":
                 answer = 01234
-              question = str(question.replace("'", ""))
+              question = str(question)
               print "ADDING CALL {} {} {} {} to the DB".format(user_id, call_id, question, answer)
               q_call_array = '''INSERT INTO calls(user_id, call_id, question, answer)
                                 VALUES (%(user_id)s, %(call_id)s, %(question)s, %(answer)s)'''
