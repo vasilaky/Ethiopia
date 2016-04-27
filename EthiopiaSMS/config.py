d1d9c2dfd7f410cbb19514253dcdee6dcaf57fc1
Eunice Kokor <eek2138@columbia.edu>
2016-04-27 11:00:03 -0400

    fixed from number in config

--- EthiopiaSMS/config.py
+++ EthiopiaSMS/config.py
@@ -5,8 +5,8 @@ logging.basicConfig(level=logging.INFO)
 
 ACCOUNT_SID = os.environ["ACCOUNT_SID"]
 AUTH_TOKEN = os.environ["AUTH_TOKEN"]
-FROM_NUMBER = os.environ["FROM_NUMBER_SA"]
-FROM_NUMBER = os.environ["FROM_NUMBER_DR"]
+FROM_NUMBER_SA = os.environ["FROM_NUMBER_SA"]
+FROM_NUMBER_DR = os.environ["FROM_NUMBER_DR"]
 DATABASE_URL = os.environ["DATABASE_URL"]
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
